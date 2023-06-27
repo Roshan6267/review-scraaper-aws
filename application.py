eed43c89c15799a54710eea3acda9efe338b8e87
Roshan6267 <123174339+Roshan6267@users.noreply.github.com>
2023-06-27 18:33:39 +0530

    modified

--- application.py
+++ application.py
@@ -4,17 +4,17 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
-app = Flask(__name__) # initializing a flask app
+application = Flask(__name__) # initializing a flask application
 
 @cross_origin()
-@app.route('/',methods=['GET'])  # route to display the home page
+@application.route('/',methods=['GET'])  # route to display the home page
 
 def homePage():
     return render_template("index.html")
 
 
 @cross_origin()
-@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+@application.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
 def index():
     if request.method == 'POST':
         try:
@@ -70,7 +70,7 @@ def index():
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
-                reviews.append(mydict)
+                reviews.applicationend(mydict)
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             print('The Exception message is: ',e)
@@ -81,6 +81,6 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(host='0.0.0.0', port=8000)
-	#app.run(debug=True)
+    application.run()
+    # application.run(host='0.0.0.0', port=8000)
+	#application.run(debug=True)
